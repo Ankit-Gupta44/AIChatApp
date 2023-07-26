71e4d11406607f3048eebe6bc1543703496c42a3
Ankit_Gupta <116659012+Ankit-Gupta44@users.noreply.github.com>
2023-07-26 14:32:10 +0530

    Update firebaseHelper.js

--- utils/firebaseHelper.js
+++ utils/firebaseHelper.js
@@ -5,14 +5,7 @@ import { initializeApp } from 'firebase/app'
 
 export const getFirebaseApp = () => {
     const firebaseConfig = {
-        apiKey: "AIzaSyAa8LXKN8QgoRXVTmR_cQZkEe7BymqS300",
-        authDomain: "chatbot-1045c.firebaseapp.com",
-        databaseURL: "https://chatbot-1045c-default-rtdb.firebaseio.com",
-        projectId: "chatbot-1045c",
-        storageBucket: "chatbot-1045c.appspot.com",
-        messagingSenderId: "151170709025",
-        appId: "1:151170709025:web:8b4f08786607dbf0ebd560",
-        measurementId: "G-W1JHM13BMJ"
+        // Use Your firebase configuration
     }
 
     // Initialize Firebase
